83edfd4530819932a4342a39d596c69bcc0d41a6
github username <github email address>
2018-08-28 13:08:50 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,5 +1,18 @@
 require_relative 'config/environment'
+require_relative 'models/puppy.rb'
 
 class App < Sinatra::Base
+  get '/' do 
+    @page_title = "Index"
+    erb :index
+  end
 
+  get '/new' do
+    erb :create_puppy
+  end
+
+  post '/new' do
+    @puppy = Puppy.new("\n#{params[:name]}", "\n#{params[:breed]}", "\n#{params[:age]}")
+    erb :display_puppy
+  end
 end
